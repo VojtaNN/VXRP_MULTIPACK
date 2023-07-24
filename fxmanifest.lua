b94a027cffa07b75fa15e9ad657459757d024e39
VojtaNN <61760213+VojtaNN@users.noreply.github.com>
2023-07-24 15:58:46 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,12 +3,13 @@ games { 'gta5' }
 
 author 'VojtaN'
 description 'VXRP Multipack'
-version '1.0.0'
+version '1.0.1'
 
 client_script "client/*.lua"
 
 server_script { "server/*.lua",
-                "server.lua"
+                "server.lua",
+                "update_checker.lua"
               }
 
 shared_script 'config.lua'
